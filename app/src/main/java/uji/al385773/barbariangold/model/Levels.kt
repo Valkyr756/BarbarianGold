d590343a1ce2a0759cbab7b0db08f94d4c75bcaa
Daniel Garin <al385773@uji.es>
2022-04-12 17:11:54 +0200

    Commit. Auxiliary classes

---
+++ app/src/main/java/uji/al385773/barbariangold/model/Levels.kt
@@ -0,0 +1,15 @@
+package uji.al385773.barbariangold.model
+
+object Levels {
+    val all = arrayOf(
+        Maze (
+            arrayOf(
+                "#######",
+                "#O.P..#",
+                "#######",
+                "#HH HH#",
+                "#######"
+            )
+        ),
+    )
+}
--- app/src/main/java/uji/al385773/barbariangold/model/Model.kt
+++ app/src/main/java/uji/al385773/barbariangold/model/Model.kt
@@ -1,4 +1,7 @@
 package uji.al385773.barbariangold.model
 
 class Model {
+
+    var maze: Maze = Levels.all[0]
+        private set
 }

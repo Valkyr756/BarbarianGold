da44c747203985544767db2eb33a41e835b439fa
Daniel Garin <al385773@uji.es>
2022-04-12 15:28:38 +0200

    Initial commit

---
+++ app/src/main/java/uji/al385773/barbariangold/MainActivity.kt
@@ -0,0 +1,11 @@
+package uji.al385773.barbariangold
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
